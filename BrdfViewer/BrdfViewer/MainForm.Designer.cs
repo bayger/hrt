54de7cbea29e82f85007b679f886479bdc8c65e6
Patryk Bajer <patryk.bajer@gmail.com>
2012-04-28 00:29:42 +0200

    Form1 -> MainForm

--- BrdfViewer/BrdfViewer/MainForm.Designer.cs
+++ BrdfViewer/BrdfViewer/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BrdfViewer
 {
-  partial class Form1
+  partial class MainForm
   {
     /// <summary>
     /// Required designer variable.
@@ -28,9 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
-      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
       this.label1 = new System.Windows.Forms.Label();
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.button1 = new System.Windows.Forms.Button();
@@ -38,23 +36,21 @@
       this.textBox2 = new System.Windows.Forms.TextBox();
       this.groupBox1 = new System.Windows.Forms.GroupBox();
       this.groupBox2 = new System.Windows.Forms.GroupBox();
-      this.label3 = new System.Windows.Forms.Label();
-      this.radioButton1 = new System.Windows.Forms.RadioButton();
-      this.radioButton2 = new System.Windows.Forms.RadioButton();
-      this.label4 = new System.Windows.Forms.Label();
-      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-      this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-      this.label5 = new System.Windows.Forms.Label();
+      this.button2 = new System.Windows.Forms.Button();
       this.label6 = new System.Windows.Forms.Label();
       this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-      this.button2 = new System.Windows.Forms.Button();
+      this.label5 = new System.Windows.Forms.Label();
+      this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+      this.label4 = new System.Windows.Forms.Label();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.label3 = new System.Windows.Forms.Label();
       this.groupBox1.SuspendLayout();
       this.groupBox2.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
       this.SuspendLayout();
       // 
       // label1
@@ -123,7 +119,6 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
       this.groupBox2.Controls.Add(this.button2);
-      this.groupBox2.Controls.Add(this.chart1);
       this.groupBox2.Controls.Add(this.label6);
       this.groupBox2.Controls.Add(this.numericUpDown3);
       this.groupBox2.Controls.Add(this.label5);
@@ -140,90 +135,14 @@
       this.groupBox2.TabStop = false;
       this.groupBox2.Text = "BRDF Data Generation";
       // 
-      // label3
+      // button2
       // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(16, 25);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(85, 13);
-      this.label3.TabIndex = 0;
-      this.label3.Text = "Generation type:";
-      // 
-      // radioButton1
-      // 
-      this.radioButton1.AutoSize = true;
-      this.radioButton1.Checked = true;
-      this.radioButton1.Location = new System.Drawing.Point(107, 23);
-      this.radioButton1.Name = "radioButton1";
-      this.radioButton1.Size = new System.Drawing.Size(149, 17);
-      this.radioButton1.TabIndex = 1;
-      this.radioButton1.TabStop = true;
-      this.radioButton1.Text = "Constant incidence vector";
-      this.radioButton1.UseVisualStyleBackColor = true;
-      // 
-      // radioButton2
-      // 
-      this.radioButton2.AutoSize = true;
-      this.radioButton2.Location = new System.Drawing.Point(262, 23);
-      this.radioButton2.Name = "radioButton2";
-      this.radioButton2.Size = new System.Drawing.Size(144, 17);
-      this.radioButton2.TabIndex = 2;
-      this.radioButton2.Text = "Constant reflected vector";
-      this.radioButton2.UseVisualStyleBackColor = true;
-      // 
-      // label4
-      // 
-      this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(16, 51);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(60, 13);
-      this.label4.TabIndex = 3;
-      this.label4.Text = "Angle step:";
-      // 
-      // numericUpDown1
-      // 
-      this.numericUpDown1.DecimalPlaces = 2;
-      this.numericUpDown1.Increment = new decimal(new int[] {
-            5,
-            0,
-            0,
-            131072});
-      this.numericUpDown1.Location = new System.Drawing.Point(107, 46);
-      this.numericUpDown1.Name = "numericUpDown1";
-      this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-      this.numericUpDown1.TabIndex = 4;
-      this.numericUpDown1.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            65536});
-      // 
-      // numericUpDown2
-      // 
-      this.numericUpDown2.DecimalPlaces = 2;
-      this.numericUpDown2.Increment = new decimal(new int[] {
-            1,
-            0,
-            0,
-            65536});
-      this.numericUpDown2.Location = new System.Drawing.Point(107, 72);
-      this.numericUpDown2.Name = "numericUpDown2";
-      this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-      this.numericUpDown2.TabIndex = 5;
-      this.numericUpDown2.Value = new decimal(new int[] {
-            30,
-            0,
-            0,
-            0});
-      // 
-      // label5
-      // 
-      this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(16, 74);
-      this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(77, 13);
-      this.label5.TabIndex = 6;
-      this.label5.Text = "Incident angle:";
+      this.button2.Location = new System.Drawing.Point(508, 71);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 10;
+      this.button2.Text = "Generate";
+      this.button2.UseVisualStyleBackColor = true;
       // 
       // label6
       // 
@@ -252,31 +171,90 @@
             0,
             0});
       // 
-      // chart1
+      // label5
       // 
-      this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-      chartArea1.Name = "ChartArea1";
-      this.chart1.ChartAreas.Add(chartArea1);
-      this.chart1.Location = new System.Drawing.Point(19, 111);
-      this.chart1.Name = "chart1";
-      series1.ChartArea = "ChartArea1";
-      series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-      series1.Name = "Series1";
-      this.chart1.Series.Add(series1);
-      this.chart1.Size = new System.Drawing.Size(564, 308);
-      this.chart1.TabIndex = 9;
-      this.chart1.Text = "chart1";
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(16, 74);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(77, 13);
+      this.label5.TabIndex = 6;
+      this.label5.Text = "Incident angle:";
       // 
-      // button2
+      // numericUpDown2
       // 
-      this.button2.Location = new System.Drawing.Point(508, 71);
-      this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(75, 23);
-      this.button2.TabIndex = 10;
-      this.button2.Text = "Generate";
-      this.button2.UseVisualStyleBackColor = true;
+      this.numericUpDown2.DecimalPlaces = 2;
+      this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+      this.numericUpDown2.Location = new System.Drawing.Point(107, 72);
+      this.numericUpDown2.Name = "numericUpDown2";
+      this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+      this.numericUpDown2.TabIndex = 5;
+      this.numericUpDown2.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+      // 
+      // numericUpDown1
+      // 
+      this.numericUpDown1.DecimalPlaces = 2;
+      this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+      this.numericUpDown1.Location = new System.Drawing.Point(107, 46);
+      this.numericUpDown1.Name = "numericUpDown1";
+      this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+      this.numericUpDown1.TabIndex = 4;
+      this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(16, 51);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(60, 13);
+      this.label4.TabIndex = 3;
+      this.label4.Text = "Angle step:";
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(262, 23);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(144, 17);
+      this.radioButton2.TabIndex = 2;
+      this.radioButton2.Text = "Constant reflected vector";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Checked = true;
+      this.radioButton1.Location = new System.Drawing.Point(107, 23);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(149, 17);
+      this.radioButton1.TabIndex = 1;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Constant incidence vector";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(16, 25);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(85, 13);
+      this.label3.TabIndex = 0;
+      this.label3.Text = "Generation type:";
       // 
       // Form1
       // 
@@ -293,10 +271,9 @@
       this.groupBox1.PerformLayout();
       this.groupBox2.ResumeLayout(false);
       this.groupBox2.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
       this.ResumeLayout(false);
 
     }
@@ -310,7 +287,6 @@
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.GroupBox groupBox1;
     private System.Windows.Forms.GroupBox groupBox2;
-    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     private System.Windows.Forms.Label label6;
     private System.Windows.Forms.NumericUpDown numericUpDown3;
     private System.Windows.Forms.Label label5;
--- BrdfViewer/BrdfViewer/Program.cs
+++ BrdfViewer/BrdfViewer/Program.cs
@@ -15,7 +15,7 @@ namespace BrdfViewer
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+      Application.Run(new MainForm());
     }
   }
 }
